f5a00bc85b4efb2d81e8a1aed7b61dd435ce244b
sipheng2013it <100404385+sipheng2013it@users.noreply.github.com>
2022-10-14 14:54:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IILAOTECH</title>
+    <title>IPLAOTECH</title>
 </head>
 <body>
-    <h1>Hi My name is Sipheng </h1>
+    <h1>ABOUT ME</H1>
+    <h2>Hi My name is Sipheng </h2>
     <p>i'm a programmer for APP and Web Fullstack!</p>
-    <img src="image/148-new.jpg" with="80" height="100" >My picture </img>
+    <img src="image/148-new.jpg" with="80" height="100" ><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem nisi iusto optio est corrupti similique, tempora laudantium? Excepturi labore suscipit ullam amet id laborum sapiente iusto ipsa soluta sed animi et maiores deserunt, ea recusandae nisi eveniet impedit quidem neque doloribus placeat, at consequatur error sit? Eos deserunt accusamus debitis praesentium earum soluta molestiae, dolorum voluptate natus laborum quam eveniet molestias maxime velit voluptas aut dolore officia! Perferendis, quibusdam inventore amet tempora sunt iste consectetur aspernatur labore ex beatae corrupti ad consequuntur reprehenderit sapiente. Dolorem nisi quas voluptates assumenda, voluptatum itaque magni omnis, optio sit eveniet odio id quae porro maiores cumque sapiente, tenetur deserunt aliquid minus minima molestias illum fugit! Earum inventore odit est asperiores doloribus repellendus maxime quisquam accusantium dicta voluptatum quas id voluptates dolore vel porro, ullam possimus esse obcaecati incidunt? Nemo earum aspernatur ratione suscipit vitae, omnis ad voluptatibus, cumque, veniam illum tenetur reiciendis enim ipsa cum. Est eum earum sed facere laudantium! Quam, suscipit illo nam minima vel architecto omnis repellendus non sint quia nemo nulla illum, modi aut neque exercitationem tempora ipsum explicabo reprehenderit, error voluptatem at facere maiores delectus. Quos illum magnam nesciunt aut ratione ad alias, numquam repellat reiciendis, eveniet, minus nobis neque quo at ea commodi? Ea laudantium quod ab perferendis at repudiandae praesentium accusantium laborum ex, deleniti, odio obcaecati veniam quaerat consequuntur commodi. Cupiditate reprehenderit labore impedit quaerat doloremque, obcaecati minima animi optio perferendis consequuntur maxime quos, fugit cumque, voluptas ab ipsum. Dolores atque quis repellat vel, nisi facilis odio! Rerum optio dolorum voluptas mollitia odit aspernatur est tempore voluptatibus nesciunt! Corrupti placeat cum ut quae animi officiis sunt mollitia, nihil, non accusantium minus voluptatum magni dolores hic, obcaecati eos. Molestiae quaerat laboriosam placeat excepturi illum laudantium asperiores. Neque dolore quo eos natus, similique sint laudantium odio fuga aliquam voluptas.</p></img>
 </body>
 </html>
